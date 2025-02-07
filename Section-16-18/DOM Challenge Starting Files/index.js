9d5e6da9ea8344a9f22ccfed99ba06025a771df8
DkshLuvsDucks <jai102938@gmail.com>
2025-02-07 10:37:57 +0530

    Complete Till Section-18 (DOM)!

---
+++ Section-16-18/DOM Challenge Starting Files/index.js
@@ -0,0 +1,8 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML="Changed";
+document.getElementsByTagName("li")[2].style.color = "green";
+document.querySelector("li a").style.color="red";
+document.querySelector("button").style.backgroundColor="green";
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").classList.toggle("huge");
+document.querySelector("h1").innerHTML="<em>Good Job</em>"
+document.querySelector("a").setAttribute("href","https://instagram.com");
